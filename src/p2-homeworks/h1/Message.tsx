d5863ddd59d280d56f543a54daf3b64883c367fe
DubrovskayaDaya <dashadubrovskaya1999@gmail.com>
2022-04-09 11:01:49 +0300

    First HWR

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import s from './Message.module.css'
+
+type MessageType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+
+}
+
+function Message(props: MessageType) {
+    return (
+        <div className={s.parent}>
+            <div>
+                <img className={s.avatar} src={props.avatar}/>
+            </div>
+            <div className={s.message}>
+                <div className={s.name}>{props.name}</div>
+                {props.message}
+                <div className={s.time}>{props.time}</div>
+            </div>
+        </div>
+    )
+}
+
+export default Message
