c31eba88e2218d672c9594c1f32ffaf182b98a5b
DubrovskayaDaya <dashadubrovskaya1999@gmail.com>
2022-05-04 18:08:16 +0300

    5th homework with Links (react-router-dom v6)

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
+import s from './pages/Links.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.header}>
+            <span><NavLink to={PATH.PRE_JUNIOR} className={({isActive})=>isActive? 'active':'default-link'}>PreJun </NavLink></span>
+            <span><NavLink to={PATH.JUNIOR} className={({isActive})=>isActive? 'active':'default-link'}>Jun </NavLink></span>
+            <span><NavLink to={PATH.JUNIOR_PLUS} className={({isActive})=>isActive? 'active':'default-link'}>JunPlus </NavLink></span>
         </div>
     )
 }
-
+// style={(param)=>({color: param.isActive ? 'purple':'grey'})
 export default Header
